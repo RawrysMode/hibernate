5ad00ba1e01593f700b6409c300c6bce86aef821
RawrysMode <iamisaacmu@gmail.com>
2023-01-19 22:34:54 +0700

    Create CustomColors.java

---
+++ src/main/java/com/rawrysmode/assets/colors/CustomColors.java
@@ -0,0 +1,9 @@
+package com.rawrysmode.assets.colors;
+
+import java.awt.*;
+
+public interface CustomColors {
+    Color LIGHT_GREY_COLOR = new Color(0xA9B7C6);
+    Color GREY_COLOR = new Color(0x3C3F41);
+    Color DARK_GREY_COLOR = new Color(0x2b2b2b);
+}
